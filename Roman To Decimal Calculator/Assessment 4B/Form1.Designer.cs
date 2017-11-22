3246f4f0516f122e331864f079fc648ec153b61f
Ahmed Matoussi <ahmedgeek1@gmail.com>
2017-11-22 15:30:48 -0600

    Add files via Upload
    Adding the complete directory + files of the project.

---
+++ Roman To Decimal Calculator/Assessment 4B/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Assessment_4B
+{
+    partial class romanToDecimalConverterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.lblAnswerPrompt = new System.Windows.Forms.Label();
+            this.lblRomanNumeralPrompt = new System.Windows.Forms.Label();
+            this.txtBoxRomanNumeralInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(689, 405);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 16);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "© Ahmed Matoussi";
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.Location = new System.Drawing.Point(152, 32);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(431, 24);
+            this.lblWelcome.TabIndex = 16;
+            this.lblWelcome.Text = "Welcome to our Roman-Decimal Calculator ";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Firebrick;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(431, 338);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(137, 39);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Firebrick;
+            this.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.ForeColor = System.Drawing.Color.White;
+            this.btnReset.Location = new System.Drawing.Point(213, 338);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(137, 39);
+            this.btnReset.TabIndex = 14;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.Color.Firebrick;
+            this.btnConvert.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.ForeColor = System.Drawing.Color.White;
+            this.btnConvert.Location = new System.Drawing.Point(317, 259);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(137, 39);
+            this.btnConvert.TabIndex = 13;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.BackColor = System.Drawing.Color.White;
+            this.lblAnswer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(382, 160);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(228, 43);
+            this.lblAnswer.TabIndex = 12;
+            this.lblAnswer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAnswerPrompt
+            // 
+            this.lblAnswerPrompt.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswerPrompt.Location = new System.Drawing.Point(106, 170);
+            this.lblAnswerPrompt.Name = "lblAnswerPrompt";
+            this.lblAnswerPrompt.Size = new System.Drawing.Size(100, 23);
+            this.lblAnswerPrompt.TabIndex = 11;
+            this.lblAnswerPrompt.Text = "Answer:";
+            this.lblAnswerPrompt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblRomanNumeralPrompt
+            // 
+            this.lblRomanNumeralPrompt.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRomanNumeralPrompt.Location = new System.Drawing.Point(43, 107);
+            this.lblRomanNumeralPrompt.Name = "lblRomanNumeralPrompt";
+            this.lblRomanNumeralPrompt.Size = new System.Drawing.Size(228, 23);
+            this.lblRomanNumeralPrompt.TabIndex = 10;
+            this.lblRomanNumeralPrompt.Text = "Enter a Roman numeral:";
+            this.lblRomanNumeralPrompt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtBoxRomanNumeralInput
+            // 
+            this.txtBoxRomanNumeralInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxRomanNumeralInput.Location = new System.Drawing.Point(382, 105);
+            this.txtBoxRomanNumeralInput.Name = "txtBoxRomanNumeralInput";
+            this.txtBoxRomanNumeralInput.Size = new System.Drawing.Size(228, 26);
+            this.txtBoxRomanNumeralInput.TabIndex = 9;
+            // 
+            // romanToDecimalConverterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(187)))), ((int)(((byte)(204)))));
+            this.ClientSize = new System.Drawing.Size(835, 441);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblWelcome);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblAnswerPrompt);
+            this.Controls.Add(this.lblRomanNumeralPrompt);
+            this.Controls.Add(this.txtBoxRomanNumeralInput);
+            this.Name = "romanToDecimalConverterForm";
+            this.Text = "Roman To Decimal Converter Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Label lblAnswerPrompt;
+        private System.Windows.Forms.Label lblRomanNumeralPrompt;
+        private System.Windows.Forms.TextBox txtBoxRomanNumeralInput;
+    }
+}
+
---
+++ Roman To Decimal Calculator/Assessment 4B/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assessment_4B
+{
+    public partial class romanToDecimalConverterForm : Form
+    {
+        public romanToDecimalConverterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            //declare an array of strings of the Roman Numeral letters between [I..X]
+            string[] romanNumeralLetters = {
+    "I",
+    "II",
+    "III",
+    "IV",
+    "V",
+    "VI",
+    "VII",
+    "VIII",
+    "IX",
+    "X"
+   };
+            // declare a boolean variable to verify it later is the entry is roman number! 
+            Boolean isRomanNumber;
+            // transfer the string entered to upper case and store it in a variable
+            string upStrRomanNumeral = txtBoxRomanNumeralInput.Text.ToUpper();
+            //Test if the textbox field empty
+            if (txtBoxRomanNumeralInput.Text == "")
+            {
+                MessageBox.Show("Please enter a Roman number! This value cannot be empty.");
+                lblAnswer.Text = "";
+                txtBoxRomanNumeralInput.Focus();
+
+            }
+            else
+            {
+                //verify if the array contains the value entered!
+                if (romanNumeralLetters.Contains(upStrRomanNumeral))
+                {
+                    //if the value entered is a roman number then the variable isRomanNumber is set to true
+                    isRomanNumber = true;
+                }
+                else
+                {
+                    //if the value entered is not in the roman number array, then the variable isRomanNumber is set to false
+                    isRomanNumber = false;
+                }
+
+                //if the entry is not roman number 
+                if (!isRomanNumber)
+                {
+                    //alert the user to enter a valid Roman number in the range [I..X]
+                    MessageBox.Show("Please enter a valid Roman number! included between [I..X]");
+                    txtBoxRomanNumeralInput.Text = ""; //empty the "txtBoxRomanNumeralInput" in case it has something
+                    lblAnswer.Text = ""; //empty the "lblAnswer" in case it has something
+                    txtBoxRomanNumeralInput.Focus(); //focus on the "txtBoxRomanNumeralInput" 
+
+                }
+                //if the entry is a Roman Number
+                else if (isRomanNumber)
+                {
+                    switch (upStrRomanNumeral) //switch by case of the letter entered
+                    {
+                        case "I": //if the value entered "upStrRomanNumeral" is I then 
+                            lblAnswer.Text = "1"; //show in the answer label the decimal value of I
+                            break;
+                        case "II":
+                            lblAnswer.Text = "2";
+                            break;
+                        case "III":
+                            lblAnswer.Text = "3";
+                            break;
+                        case "IV":
+                            lblAnswer.Text = "4";
+                            break;
+                        case "V":
+                            lblAnswer.Text = "5";
+                            break;
+                        case "VI":
+                            lblAnswer.Text = "6";
+                            break;
+                        case "VII":
+                            lblAnswer.Text = "7";
+                            break;
+                        case "VIII":
+                            lblAnswer.Text = "8";
+                            break;
+                        case "IX":
+                            lblAnswer.Text = "9";
+                            break;
+                        case "X":
+                            lblAnswer.Text = "10";
+                            break;
+                        default:
+                            lblAnswer.Text = "Not valid value entered!"; //entry not valid! Something went wrong
+                            break;
+                    }
+
+                }
+
+            }
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            //on click of the Reset button
+            txtBoxRomanNumeralInput.Text = ""; //empty the entered field
+            txtBoxRomanNumeralInput.Focus(); //focus on the "txtBoxRomanNumeralInput" field
+            lblAnswer.Text = ""; //empty the answer in case there is old value in it
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //close the form
+            this.Close();
+
+        }
+    }
+}
